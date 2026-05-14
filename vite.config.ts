5145d7e8d34fcf48a17a09a01a5be72a7dbdf8e6
Jonathas <jonathaspereira673@gmail.com>
2026-05-14 14:32:07 -0300

    feat: Novo Dominio

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import vitePrerender from "vite-prerender-plugin";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -10,12 +9,6 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
-
-      vitePrerender({
-        staticDir: "dist",
-        routes: ["/"],
-      }),
-
       {
         name: "api-middleware",
         configureServer(server) {
